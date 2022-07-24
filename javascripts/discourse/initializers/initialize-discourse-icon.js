781815198a0c9d883ea2b9bafe8356ee843bf2f0
Jarek Radosz <jradosz@gmail.com>
2022-07-24 17:08:00 +0200

    DEV: Clean up the code
    * de-jQ
    * early return
    * proper import of a long-established module
    * optional chaining operator

--- javascripts/discourse/initializers/initialize-discourse-icon.js
+++ javascripts/discourse/initializers/initialize-discourse-icon.js
@@ -1,30 +1,34 @@
 import { escapeExpression } from "discourse/lib/utilities";
-import { renderIcon } from "discourse-common/lib/icon-library";
+import {
+  isExistingIconId,
+  renderIcon,
+} from "discourse-common/lib/icon-library";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { htmlSafe } from "@ember/template";
 
-function _attachIcons($cooked) {
-  const icons = $cooked[0].querySelectorAll(".d-wrap[data-wrap=icon]");
+function _attachIcons(cooked) {
+  const icons = cooked.querySelectorAll(".d-wrap[data-wrap=icon]");
+
   icons.forEach((icon) => {
     const id = escapeExpression(icon.dataset.id);
     const params = { translatedTitle: id };
-    const isExistingIconId =
-      require("discourse-common/lib/icon-library").isExistingIconId;
 
-    if (!isExistingIconId || (isExistingIconId && isExistingIconId(id))) {
+    if (isExistingIconId(id)) {
       icon.innerHTML = htmlSafe(renderIcon("string", id, params));
-    } else {
-      const img = document.createElement("img");
-      img.classList.add("svg-as-img");
-      let color = getComputedStyle(document.body).getPropertyValue("--primary");
-      color && color.length
-        ? (color = color.replace("#", ""))
-        : (color = "000000");
-      img.src = `/svg-sprite/${
-        window.location.hostname
-      }/icon/${color.trim()}/${id}.svg`;
-      icon.replaceWith(img);
+      return;
     }
+
+    const color =
+      getComputedStyle(document.body)
+        .getPropertyValue("--primary")
+        ?.replace("#", "")
+        .trim() || "000000";
+
+    const img = document.createElement("img");
+    img.classList.add("svg-as-img");
+    img.src = `/svg-sprite/${window.location.hostname}/icon/${color}/${id}.svg`;
+
+    icon.replaceWith(img);
   });
 }
 
@@ -33,7 +37,7 @@ export default {
 
   initialize() {
     withPluginApi("0.8.7", (api) => {
-      api.decorateCooked(_attachIcons, {
+      api.decorateCookedElement(_attachIcons, {
         id: "discourse-icon",
       });
     });
