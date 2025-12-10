6b1a95d5b00fed4176b2601d79c1c0a8280e8410
Bryce Huhtala <100382978+sketchius@users.noreply.github.com>
2025-12-09 21:55:46 -0500

    A11Y: add label with translation key

--- javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
+++ javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
@@ -6,6 +6,7 @@ export default apiInitializer((api) => {
       event.addText("[wrap=icon id=heart][/wrap]");
     },
     group: "insertions",
+    label: themePrefix("toolbar.insert"),
     icon: "discourse-emojis",
   });
 });
