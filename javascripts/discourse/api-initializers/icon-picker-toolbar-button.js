242cfb0ba03f419c30a8392fdac3c7cb9e820128
Bryce Huhtala <100382978+sketchius@users.noreply.github.com>
2025-12-09 21:52:11 -0500

    DEV: use apiInitializer

--- javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
+++ javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
@@ -1,17 +1,11 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
+import { apiInitializer } from "discourse/lib/api";
 
-export default {
-  name: "icon-composer-button",
-
-  initialize() {
-    withPluginApi((api) => {
-      api.addComposerToolbarPopupMenuOption({
-        action(event) {
-          event.addText("[wrap=icon id=heart][/wrap]");
-        },
-        group: "insertions",
-        icon: "discourse-emojis",
-      });
-    });
-  },
-};
+export default apiInitializer((api) => {
+  api.addComposerToolbarPopupMenuOption({
+    action(event) {
+      event.addText("[wrap=icon id=heart][/wrap]");
+    },
+    group: "insertions",
+    icon: "discourse-emojis",
+  });
+});
