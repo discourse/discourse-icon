80e6a25505bf6978df8a24685e384201d336d1c0
Bryce Huhtala <100382978+sketchius@users.noreply.github.com>
2025-12-09 22:20:35 -0500

    DEV: show modal on toolbar btn

--- javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
+++ javascripts/discourse/api-initializers/icon-picker-toolbar-button.js
@@ -1,8 +1,13 @@
 import { apiInitializer } from "discourse/lib/api";
+import IconPickerModal from "../components/modal/icon-picker.gjs";
 
 export default apiInitializer((api) => {
   api.addComposerToolbarPopupMenuOption({
     action(event) {
+      const modal = api.container.lookup("service:modal");
+      modal.show(IconPickerModal, {
+        model: {}
+      });
       event.addText("[wrap=icon id=heart][/wrap]");
     },
     group: "insertions",
